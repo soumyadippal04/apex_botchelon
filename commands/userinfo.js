821a0e41c01ac08b2dc71dd06b5cb12d4f8b7126
Soumil07 <soumil.paranjpay@gmail.com>
2017-07-28 21:19:42 +0530

    Fixed userinfo
    userinfo had a few errors. New refracted system provides basic user info in an embed.
    
    TO DO LIST:
    
    Prettify current embed
    Make it mentionable

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,24 +1,31 @@
 const Discord = require('discord.js')
-exports.run = (client, msg, args) => {
+exports.run = async(client, msg) => {
   const embed = new Discord.RichEmbed()
-  .setAuthor(msg.author.username)
-  .setColor(0x00AE86);
-  .setThumbnail(msg.author.avatar);
-  .addField('Joined Discord on', `${msg.author.createdAt}`);
+  .setAuthor(msg.author.username, msg.author.avatarURL)
+  .setColor(3447003)
+  .setDescription(`Is a member in ${msg.guild.name}`)
+  .addField('Joined discord', msg.author.createdAt.toLocaleString(), true)
+  .addField(`Joined ${msg.guild.name}`, msg.member.joinedAt.toLocaleString(), true)
   .setTimestamp()
-  msg.channel.send(embed)
-}
+  //command currently only works for the author
 
+  try {
+    await msg.channel.send({embed})
+  } catch(e) {
+    console.log(e)
+  }
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: ['ui'],
   permLevel: 0
-};
+}
 
 exports.help = {
   name: 'userinfo',
-  description: 'Displays user information & statistics',
+  description: 'Shows guild information on the mentioned user',
   usage: 'userinfo'
-};
+}
+
