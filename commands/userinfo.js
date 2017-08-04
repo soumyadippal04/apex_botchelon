2c238f3cd64fd3566c5be9eb38c411502b2a271a
Soumil07 <soumil.paranjpay@gmail.com>
2017-08-04 12:41:03 +0530

    UI now mentionable

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,21 +1,17 @@
 const Discord = require('discord.js')
 exports.run = async(client, msg) => {
+const member = msg.mentions.members.first() || msg.member;
   const embed = new Discord.RichEmbed()
-  .setAuthor(msg.author.username, msg.author.avatarURL)
-  .setColor(3447003)
-  .setDescription(`Is a member in ${msg.guild.name}`)
-  .addField('Joined discord', msg.author.createdAt.toLocaleString(), true)
-  .addField(`Joined ${msg.guild.name}`, msg.member.joinedAt.toLocaleString(), true)
-  .addField('Roles', msg.member.roles.map(r => r.name).join(", "))
-  .setTimestamp()
-  //command currently only works for the author
-
-  try {
-    await msg.channel.send({embed})
-  } catch(e) {
-    console.log(e)
-  }
-};
+    .setColor(3447003)
+    .setTimestamp()
+    .setFooter(client.user.username, client.user.avatarURL)
+    .setAuthor(member.user.username)
+    .setDescription(`Is a member in ${msg.guild.name}. Status: ${member.user.presence.status}`)
+    .addField(`Joined Discord`, member.user.createdAt.toLocaleString(), true)
+    .addField(`Joined ${msg.guild.name}`, member.joinedAt.toLocaleString(), true)
+    .addField('Roles', member.roles.map(r => r.name).join(", "));
+  msg.channel.send({embed});
+}
 
 exports.conf = {
   enabled: true,
@@ -27,6 +23,6 @@ exports.conf = {
 exports.help = {
   name: 'userinfo',
   description: 'Shows guild information on the mentioned user',
-  usage: 'userinfo'
+  usage: 'userinfo [mention]'
 }
 
